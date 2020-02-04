72f9bedc6c0de572baa4cb94f07ec6e2a7d7f048
cnfreelancer.com <9570075@qq.com>
2020-02-04 20:59:16 +0800

    loginForm UI n dataBind

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//导入图标样式
+import './assets/fonts/iconfont.css'
+// 导入全局CSS样式
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
