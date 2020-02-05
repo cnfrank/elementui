df06f1f76e1b9622ebc6b26c2e079c1f84628df4
cnfreelancer.com <9570075@qq.com>
2020-02-05 15:56:37 +0800

    prettier

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,4 +1,4 @@
-<template >
+<template>
   <div class="login_container">
     <div class="login_box">
       <!--头像区域-->
@@ -6,10 +6,18 @@
         <img src="../assets/logo.png" alt />
       </div>
       <!--Form区域-->
-      <el-form ref="loginFormRef" :model="loginForm" :rules="loginFormRules" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        class="login_form"
+      >
         <!--用户名-->
         <el-form-item prop="username">
-          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
         </el-form-item>
         <!--密码-->
         <el-form-item prop="password">
@@ -22,7 +30,7 @@
 
         <!--按钮区域-->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
+          <el-button type="primary" @click="validateLoginForm">登录</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
@@ -36,30 +44,49 @@ export default {
     return {
       // 这是数据绑定
       loginForm: {
-        username: "zs",
-        password: "123"
+        username: 'admin',
+        password: '123456'
       },
       // 这是数据规则
       loginFormRules: {
         // 验证username
         username: [
-          { required: true, message: "请输入登录用户名", trigger: "blur" },
-          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+          { required: true, message: '请输入登录用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
         ],
         // 验证password
         password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
         ]
       }
     };
   },
-  methods:{
-        //点击重置表单
-      resetLoginForm(){
-
-          this.$refs.loginFormRef.resetFields();
-      }
+  methods: {
+    //点击重置表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields();
+    },
+    //点击重置表单
+    validateLoginForm() {
+      this.$refs.loginFormRef.validate(async validate => {
+        if (!validate) return;
+        // console.log('success');
+        const { data: res } = await this.$http.post('login', this.loginForm);
+        // console.log(res);
+        if (res.meta.status != 200) {
+          this.$message.error('登录失败');
+        } else {
+          this.$message.success('登录成功');
+          // 将登录之后的token值保存到客户端本地的sessionStorage中
+          // 1.1 项目中除了登录界面，其他界面都需要登录以后才能访问
+          // 1.2 token只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+          // 通过编程式导航跳转到后台主页，路由为/home
+          window.sessionStorage.setItem('token', res.data.token);
+          this.$router.push('/home');
+        }
+      });
+    }
   }
 };
 </script>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import { Button } from 'element-ui'
 import { Form, FormItem } from 'element-ui'
 import { Input } from 'element-ui'
+// 导入Message提示
+import { Message } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+
+// 挂载至vue的原型对象
+Vue.prototype.$message = Message;
